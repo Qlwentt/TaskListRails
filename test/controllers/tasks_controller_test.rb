9e16a9542731518453b39745a2ebff358582bbf5
Qlwentt <qlwentt@gmail.com>
2016-10-23 14:50:47 -0700

    begin user/session testing

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,6 +1,21 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
+  test "Make sure a user can see their tasks" do
+    session[:user_id]=users(:ada).id
+    get :show, id: tasks(:adas_task).id 
+
+    assert_response :success
+  end
+
+  test "Make sure a user cannot see another user's tasks" do
+    session[:user_id]=users(:quai).id
+    get :show, id: tasks(:adas_task).id 
+
+    assert_response :redirect
+    assert_equal flash[:notice], "You do not have access to that task"
+  end
+
   test "should get index" do
     get :index
     assert_response :success
